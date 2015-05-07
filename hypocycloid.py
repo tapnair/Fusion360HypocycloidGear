4690d0711e32a388cde0fb28cdf8a1c21c2f6fbd
tapnair <patrick.rainsberry@gmail.com>
2015-05-07 15:45:23 -0700

    Update hypocycloid.py

--- hypocycloid.py
+++ hypocycloid.py
@@ -7,10 +7,8 @@
 #"""Hypocycloid cam generator
 #Generate dxfs of hypocycloid cams for cycloid drives
 #
-#Copyright 	2009, Alex Lait
-#Version 	v0.2 (09/13/09)
 #License 	GPL
-#Homepage 	http://www.zincland.com/hypocycloid
+#Credit to: 	http://www.zincland.com/hypocycloid
 #
 #Credit to:
 #	Formulas to describe a hypocycloid cam
@@ -25,14 +23,7 @@
 #	Formulas for calculating the pressure angle and finding the limit circles
 #	http://imtuoradea.ro/auo.fmte/files-2007/MECATRONICA_files/Anamaria_Dascalescu_1.pdf
 #
-#Notes:
-#	Does not currently do ANY checking for sane input values and it
-#	is possible to create un-machinable cams, use at your own risk
-#
-#	Suggestions:
-#	- Eccentricity should not be more than the roller radius
-#	- Has not been tested with negative values, may have interesting results :)
-#"""
+
 
 import adsk.core, adsk.fusion, traceback
 import os, math
